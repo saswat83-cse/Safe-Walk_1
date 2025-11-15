5a675df9c4a2c53247e174d1f5fe744826b3953b
saswat83-cse <saswatabid@gmail.com>
2025-11-15 18:43:38 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,7 @@
+document.getElementById('getSigninBtn').onclick = function() {
+    window.open('signin.html', '_blank'); // open in new tab
+}
+
+document.getElementById('getLoginBtn').onclick = function() {
+    window.location.href = 'index.html'; // go to login page
+}
